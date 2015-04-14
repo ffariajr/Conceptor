0bbdc0512cbd2f1f0f8092cc56e3756ab3c7dbd5
Fernando Faria <fariaf1@tcnj.edu>
2015-04-13 22:55:38 -0400

    Update init.js

--- html/scripts/init.js
+++ html/scripts/init.js
@@ -1,1 +1,7 @@
+// Create canvas to fit the area
+var container = document.getElementById("conceptor-layer");
+var paper = Raphael(container, container.offsetWidth, window.innerHeight-200);
+
+// Create rectangle bounds of toolbar 
+var rect = paper.rect(-1,-1, container.offsetWidth/4, window.innerHeight-198, 0);
 
